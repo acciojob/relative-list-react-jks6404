69b10b36a2c8df91c73459374e62a9cfe037754e
jks6404 <jannusingh.75@gmail.com>
2023-08-27 09:08:29 +0530

    submitting code-Sun Aug 27 2023 09:08:27 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
+  const relatives = ["Relative 1", "Relative 2", "Relative 3", "Relative 4"];
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h3>Names of Relative Panning to Visit:</h3>
+      <ol key="relativeList" id="relativeList">
+        {relatives.map((relative, index) => (
+          <li key={`relativeListItem${index+1}`} id="relativeListItem1">{relative }</li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
